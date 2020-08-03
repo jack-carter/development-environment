f54016f261ea4f0d6a585850804b3d0b7f6d1da7
Jack Carter <the.jack.carter@gmail.com>
2020-08-03 10:26:42 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,12 +8,12 @@ set -u
 if [[ -z `which brew` ]]; then
   curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh | bash
   brew --version 
-  if [[ $? -ge 0 ]]; then
+  if [[ $? -gt 0 ]]; then
     echo "ERROR: something didn't install properly for HomeBrew"
     exit 1
   fi
   git --version
-  if [[ $? -ge 0 ]]; then
+  if [[ $? -gt 0 ]]; then
     echo "ERROR: something didn't install properly for git"
     exit 2
   fi
@@ -23,7 +23,7 @@ fi
 if [[ -z `command -v nvm` ]] && [[ -z `which nvm` ]]; then
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
   nvm --version
-  if [[ $? -ge 0 ]]; then
+  if [[ $? -gt 0 ]]; then
     echo "ERROR: something didn't install properly for nvm"
     exit 3
   fi
